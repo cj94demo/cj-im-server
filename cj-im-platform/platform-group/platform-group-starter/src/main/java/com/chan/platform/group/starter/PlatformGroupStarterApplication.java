df540ef7b8405399dae1943539ad5e122144103b
chenjun <chenjun@thpower.com>
2024-06-28 22:27:28 +0800

    群组服务更新

--- cj-im-platform/platform-group/platform-group-starter/src/main/java/com/chan/platform/group/starter/PlatformGroupStarterApplication.java
+++ cj-im-platform/platform-group/platform-group-starter/src/main/java/com/chan/platform/group/starter/PlatformGroupStarterApplication.java
@@ -1,13 +1,17 @@
 package com.chan.platform.group.starter;
 
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDubbo
+@EnableDiscoveryClient
 @SpringBootApplication
 public class PlatformGroupStarterApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PlatformGroupStarterApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PlatformGroupStarterApplication.class, args);
+    }
 
 }
